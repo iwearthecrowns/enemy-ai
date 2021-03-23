448d0ff9e5bc88cfe55209cc5fff77f2ca29ba99
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2021-03-23 00:59:45 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,54 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 1 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 3 1 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 1 1 1 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`;
+            case "image2":return img`
+a a a a a a a a a a a a a a a a 
+a b b b b b b b b b b b b b b a 
+a b a a a a a a a a a a a a b a 
+a b a a b b a a a a b b a a b a 
+a b a a a a b a a b a a a a b a 
+a b a a a a a a a a a a a a b a 
+a b a a a b a a a a b a a a b a 
+a b a a a b a a a a b a a a b a 
+a b a a a a a a a a a a a a b a 
+a b a a a a a a a a a a a a b a 
+a b a a a b b b b b b a a a b a 
+a b a a b a a a a a a b a a b a 
+a b a a a a a a a a a a a a b a 
+a b a a a a a a a a a a a a b a 
+a b b b b b b b b b b b b b b a 
+a a a a a a a a a a a a a a a a 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
